d5fca7a14caeae30489f149a9c8e640eb4f9c1ee
CaioSouza021 <caio.souza.cs.2003@outlook.com.br>
2025-04-17 12:52:28 -0300

    Finalizado

--- js/menu.js
+++ js/menu.js
@@ -10,3 +10,17 @@ document.addEventListener("scroll", () => {
     navBar.classList.remove("rolar");
   }
 });
+
+// Animação do botao menu mobile
+let btnMenuMob = document.querySelector("#btn-menu-mob");
+let line1 = document.querySelector(".line-menumob-1");
+let line2 = document.querySelector(".line-menumob-2");
+let menuMobile = document.querySelector("#menu-mobile");
+let body = document.querySelector("body");
+
+btnMenuMob.addEventListener("click", () => {
+  line1.classList.toggle("ativo1");
+  line2.classList.toggle("ativo2");
+  menuMobile.classList.toggle("abrir");
+  body.classList.toggle("block");
+});
